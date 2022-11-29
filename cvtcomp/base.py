7f543e2dd7c9942d1b75ada1ef43daa325a2076e
ilyatro <ilya_trofimov@inbox.ru>
2022-11-29 23:58:37 +0300

    add class TensorVideo, delete_reference_frame, restore_from_reference_frame, bin_search_video_metric

--- cvtcomp/base.py
+++ cvtcomp/base.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import tensorly as tl
 from tensorly.decomposition import tucker, matrix_product_state
@@ -6,7 +8,6 @@ import cv2
 
 
 def tucker_encode(data: np.ndarray, quality=1.0):
-
     ranks = (
         max(1, int(data.shape[0] * quality)),
         int(data.shape[1] * quality),
@@ -25,7 +26,6 @@ def tucker_encode(data: np.ndarray, quality=1.0):
 
 
 def tt_encode(data: np.ndarray, quality=1.0):
-
     ranks = (
         1,
         int(data.shape[0] * quality),
@@ -43,12 +43,10 @@ def tt_encode(data: np.ndarray, quality=1.0):
 
 
 def tt_decode(compressed_data: list) -> np.ndarray:
-
     return tt_to_tensor(compressed_data)
 
 
 def tucker_decode(compressed_data: list) -> np.ndarray:
-
     return tucker_to_tensor(compressed_data)
 
 
@@ -74,7 +72,7 @@ class Encoder:
                 else:
                     raise ValueError(f"Wrong encoder type: {self.encoder_type}")
             elif key == "quality":
-                assert value <= 1.0 and value >= 0, "0 <= quality <= 1"
+                assert 0 <= value <= 1.0, "0 <= quality <= 1"
                 self.quality = value
             else:
                 raise ValueError(f"Wrong argument is provided : {value}")
@@ -114,23 +112,70 @@ class Decoder:
         return decompressed_data
 
 
-    class StreamerEncoded:
-        """Class for streamming the compressed data in a frame-wise manner"""
+class StreamerEncoded:
+    """Class for streaming the compressed data in to frame-wise manner"""
 
-        def __init__(self, compressed_data, video_len, encoder_type="tucker"):
-            self.compressed_data = compressed_data
-            self.encoder_type = encoder_type
-            self.n_frame = 0
-            self.video_len = video_len
+    def __init__(self, compressed_data, video_len, encoder_type="tucker"):
+        self.compressed_data = compressed_data
+        self.encoder_type = encoder_type
+        self.n_frame = 0
+        self.video_len = video_len
 
-        def __next__(self):
-            while self.n_frame < self.video_len:
-                if self.encoder_type == 'tucker':
-                    raise NotImplementedError("TBD")
-                elif self.encoder_type == 'tt':
-                    raise NotImplementedError("TBD")
-                self.n_frame += 1
-                yield None
+    def __next__(self):
+        while self.n_frame < self.video_len:
+            if self.encoder_type == 'tucker':
+                raise NotImplementedError("TBD")
+            elif self.encoder_type == 'tt':
+                raise NotImplementedError("TBD")
+            self.n_frame += 1
+            yield None
 
-        def __iter__(self):
-            return self
+    def __iter__(self):
+        return self
+
+
+class TensorVideo:
+    def __init__(self, data=None, encoder_type='tt', quality=0.5, chunk_size=None, data_type=np.uint8):
+        self.encoder_type = encoder_type
+        self.quality = quality
+        self.encoder = Encoder(encoder_type=self.encoder_type, quality=self.quality)
+        self.decoder = Decoder(decoder_type=self.encoder_type)
+        self.chunk_size = chunk_size
+        self.shape = None
+        self.data_type = data_type
+        self.data = self.from_numpy(data) if data is not None else None
+
+    def from_numpy(self, data: np.ndarray):
+        res = []
+        self.data_type = data.dtype
+        if self.chunk_size is None:
+            self.chunk_size = data.shape[0]
+        self.shape = data.shape
+        for frame_no in range(0, data.shape[0], self.chunk_size):
+            chunk = data[frame_no:frame_no + self.chunk_size, :, :, :].astype(np.float64)
+            coded_chunk = self.encoder.encode(chunk)
+            res.append(coded_chunk)
+        return res
+
+    def to_numpy(self):
+        res = []
+        for coded_chunk in self.data:
+            x = self.decoder.decode(coded_chunk)
+            res.append(x)
+
+        res = np.vstack(res)
+
+        res[res > 255.0] = 255.0
+        res[res < 0.0] = 0.0
+        res = res.astype(self.data_type)
+        return res
+
+    def save(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.__dict__, file)
+
+    def load(self, filename):
+        with open(filename, 'rb') as file:
+            self.__dict__ = pickle.load(file)
+        return self
+
--- cvtcomp/utils.py
+++ cvtcomp/utils.py
@@ -5,8 +5,9 @@ import os
 import numpy as np
 import cv2
 import tqdm
+from matplotlib import pyplot as plt, animation
 from skimage.metrics import structural_similarity
-
+from IPython.display import HTML
 from cvtcomp.base import *
 
 
@@ -79,6 +80,16 @@ def compute_ssim(video1, video2):
     return ssim_val
 
 
+def encdec_video_chunkwise(video, encoder, decoder, chunk_size=10):
+    decompressed_video = np.zeros(video.shape, dtype=np.uint8)
+
+    for ii in range(0, video.shape[0], chunk_size):
+        compressed_chunk = encoder.encode(video[ii: ii + chunk_size, :, :, :])
+        decompressed_video[ii: ii + chunk_size, :, :, :] = decoder.decode(compressed_chunk)
+
+    return decompressed_video
+
+
 def compute_metrics_dataset(folderpath: str, encoder: Encoder, decoder: Decoder, metric="psnr") -> float:
     if metric == "psnr":
         compute_metric = cv2.PSNR
@@ -114,6 +125,103 @@ def compute_metrics_dataset(folderpath: str, encoder: Encoder, decoder: Decoder,
         res_cr[ii] = float(compressed_size) / float(total_size)
 
     return np.mean(res_metrics), np.mean(res_cr)
+
+
+def plot_ssim_ms_ssim(filepath: str, encoder_type='tucker', qualities=(0.10, 0.25, 0.50)):
+    result_ssim = []
+    video, *_ = load_video_to_numpy(filepath)
+
+    for quality in qualities:
+        decompressed_video = TensorVideo(
+            data=video,
+            encoder_type=encoder_type,
+            quality=quality,
+            chunk_size=50
+        ).to_numpy()
+
+        ssim_val = structural_similarity(video, decompressed_video, multichannel=True)
+        result_ssim.append(ssim_val)
+
+    result_ssim = np.array(result_ssim)
+    x = np.array(qualities)
+
+    plt.figure(figsize=(8, 6))
+    plt.grid(True)
+    plt.xlabel("Core size")
+    plt.ylabel("ms_ssim, ssim")
+    plt.plot(x, result_ssim)
+
+
+def convert_from_y4m_to_avi(filename, encoder_type='tucker', quality=1.0, chunk_size=50):
+    video, fourcc, fps, size = load_video_to_numpy(filename)
+    decompressed_video = TensorVideo(
+        video,
+        encoder_type=encoder_type,
+        quality=quality,
+        chunk_size=chunk_size
+    ).to_numpy()
+    save_video_from_numpy(filename[:-4] + '.avi', decompressed_video, fourcc, fps, size, color=True)
+
+
+def delete_reference_frame(video):
+    new_video = video.astype(np.int16) - video[0].astype(np.int16)
+    return new_video[1:], video[0]
+
+
+def restore_from_reference_frame(video_without_ref_frame, ref_frame):
+    video = video_without_ref_frame + ref_frame
+    video = np.vstack([ref_frame.reshape((1, ref_frame.shape[0], ref_frame.shape[1], ref_frame.shape[2])), video])
+    video[video > 255.0] = 255.0
+    video[video < 0.0] = 0.0
+    video = video.astype(np.uint8)
+    return video
+
+
+def bin_search_video_metric(video, target_metric_value, num_iter=5, metric='psnr', encoder_type='tt'):
+    if metric == "psnr":
+        compute_metric = cv2.PSNR
+    elif metric == "ssim":
+        compute_metric = compute_ssim
+    else:
+        raise ValueError(f"Wrong metric is specified: {metric}. Please , use 'psnr' or 'ssim'")
+
+    min_value = 0.0
+    max_value = 1.0
+    value, metric_value, compressed_video = 0, 0, None
+    for _ in range(num_iter):
+        value = (max_value + min_value) / 2
+        compressed_video = TensorVideo(video, encoder_type=encoder_type, quality=value, chunk_size=50).to_numpy()
+        metric_value = compute_metric(video, compressed_video)
+        print(value, metric_value)
+        if metric_value > target_metric_value:
+            max_value = value
+        else:
+            min_value = value
+
+    return value, metric_value, compressed_video
+
+
+def play_video(video: np.array, fps: int = 30):
+    """
+    Plays video specified in np.array
+    Input:
+    :param: video - np.array [frames, height, width, channels] - RGB uint8
+    :param: fps - fps :)
+    """
+    fig = plt.figure(figsize=(12,6))
+    im = plt.imshow(video[0,:,:,:])
+    plt.close() # this is required to not display the generated image
+
+    def init():
+        im.set_data(video[0,:,:,:])
+
+    def animate(i):
+        im.set_data(video[i,:,:,:])
+        return im
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=video.shape[0],
+                                   interval=1000.0 / fps)
+    return HTML(anim.to_html5_video())
 
 
 if __name__ == "__main__":
